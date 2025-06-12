8d0652b18aaf8784b40cb501d236576c834e8100
sadikhossainnub <sadik.hossain.nub@gmail.com>
2025-06-13 00:08:15 +0600

    Update cost_calculation.js

--- cost_calculation/cost_calculation/doctype/cost_calculation/cost_calculation.js
+++ cost_calculation/cost_calculation/doctype/cost_calculation/cost_calculation.js
@@ -1,8 +1,24 @@
-// Copyright (c) 2025, Md. Abu Sayed and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Cost Calculation', {
+    validate: function(frm) {
+        frm.doc.total_amount = 0;
+        $.each(frm.doc.items || [], function(i, d) {
+            frm.doc.total_amount += d.qty * d.rate;
+        });
+        refresh_field("total_amount");
+    }
+});
 
-// frappe.ui.form.on("Cost Calculation", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Cost Calculation Item', {
+    rate: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        child.amount = child.qty * child.rate;
+        refresh_field("amount", cdt, cdn);
+        frm.trigger("validate"); // Trigger the main DocType's validate function
+    },
+    qty: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        child.amount = child.qty * child.rate;
+        refresh_field("amount", cdt, cdn);
+        frm.trigger("validate"); // Trigger the main DocType's validate function
+    }
+});
